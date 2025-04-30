e442aabc360b9f3854d735859f39a7b65a181092
GHZ <57097690+keithAND2020@users.noreply.github.com>
2025-04-30 12:37:16 +0800

    Add files via upload

--- tools/trainval.py
+++ tools/trainval.py
@@ -20,15 +20,27 @@ def main():
     parser.add_argument('--resume', type=str, default=None)
     parser.add_argument('--launcher', choices=['none', 'slurm', 'pytorch'], default='none')
     parser.add_argument('--tcp_port', type=int, default=18888, help='tcp port for distrbuted training')
+
+
+    parser.add_argument('--use_loss', type=str, default=None, 
+                        help='Override the use_loss parameter in the config file (e.g., "L1", "L2")')
+    parser.add_argument('--use_attention', action='store_true', 
+                        help='Set use_att to True if this flag is provided')
     args = parser.parse_args()
     configs, logger = init_all(args)
 
-    trainloader, evalloader = build_dataloaders(**configs['dataset'])
 
+    if args.use_loss is not None:
+        configs['model']['use_loss'] = args.use_loss
+    if args.use_attention:
+        configs['model']['use_attention'] = True
+        
+    trainloader, evalloader = build_dataloaders(**configs['dataset'])
+    # pdb.set_trace()
     net = build_models(logger, **configs['model']).to('cuda')
     if args.evaluate:
         tester = build_tester(net, evalloader, **configs['test'])
-        tester.eval()
+        tester.test()
         print('done')
         exit()
 
